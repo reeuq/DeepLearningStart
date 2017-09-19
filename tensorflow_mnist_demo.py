b8d9990429b3e02aa8a7a8ff68fc41f99577888d
wyd <wangyadao@gmail.com>
2017-09-19 08:28:38 +0800

    tensorflow mnist demo add
    Signed-off-by: wyd <wangyadao@gmail.com>

---
+++ tensorflow_mnist_demo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 11:02:35 2017
+
+@author: wyd
+"""
+# create a softmax regression
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/home/wyd/TensorFlow/mnist", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# test = y.consumers()
+# print(test)
+# te, tb = mnist.test.images,mnist.test.labels
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# ddd = sess.run(correct_prediction, feed_dict={x:mnist.test.images, y_: mnist.test.labels})
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
